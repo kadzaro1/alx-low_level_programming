83d937b9b239f535aec39cf7fba421ab6c2fc79b
kadzaro1 <koblamens40@gmail.com>
2022-07-06 04:53:58 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 0; ch < 10; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
