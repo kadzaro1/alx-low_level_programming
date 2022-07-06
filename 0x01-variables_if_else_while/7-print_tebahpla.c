91b9b3470c61bf03783fb0455be91a93bfede579
kadzaro1 <koblamens40@gmail.com>
2022-07-06 06:39:24 -0700

    added 7-print_tebahpla.c and 8-print_base16.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+char ch;
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
