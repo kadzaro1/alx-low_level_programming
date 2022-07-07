b51005b77ddfa385ba71c8192cbec7bd3073ffa6
kadzaro1 <koblamens40@gmail.com>
2022-07-07 08:05:40 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - prints alphabet
  *
- * Return: Always 0 (success)
+ * _putchar: prints to the output stream
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet(void);
-return (0);
+int  ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n);
 }
--- 0x02-functions_nested_loops/print_alphabet.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void print_alphabet(void)
-{
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-	printf("%s", ch);
-printf("\n");
-}
