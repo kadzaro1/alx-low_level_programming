b973578c06cc0fa142d2115c5cc4f3898c3b1c4c
kadzaro1 <koblamens40@gmail.com>
2022-07-07 08:19:09 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * _putchar: prints to the output stream
+ */
+void print_alphabet_10x(void)
+{
+int i = 0;
+if (i <= 10)
+{
+i++
+int  ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
