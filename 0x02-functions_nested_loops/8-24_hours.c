6482c7beeee7bc129ffe5d2a8c8c7d17cd8ab466
kadzaro1 <koblamens40@gmail.com>
2022-07-09 12:01:37 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ for (m = 0; m < 60; m++)
 	_putchar('0');
 	_putchar(h + '0');
 	}
-	else (h >= 10)
+	else if (h >= 10)
 	{
 	_putchar((h / 10) + '0');
 	_putchar((h % 10) + '0');
@@ -29,9 +29,9 @@ for (m = 0; m < 60; m++)
 	_putchar('0');
 	_putchar(m + '0');
 	}
-	 else (m >= 10)
+	 else if (m >= 10)
 	{
-	_putchar(':')
+	_putchar(':');
 	_putchar((m / 10) + '0');
 	_putchar((m % 10) + '0');
 	}
