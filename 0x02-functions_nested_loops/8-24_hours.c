139e9ada57af11738e408799798b943a0a514a0a
kadzaro1 <koblamens40@gmail.com>
2022-07-09 11:04:07 -0700

    added 8-24_hours.c and 9-times_table.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute from 00:00 to 23:59
+ *
+ * _putchar: prints character to the std output
+ */
+void jack_bauer(void)
+{
+int m;
+int h;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+	{
+	if (h < 10)
+	{
+	_putchar('0');
+	_putchar(h + '0');
+	}
+	/**
+	 *else (h >= 10)
+	{
+	_putchar((h / 10) + '0');
+	_putchar((h % 10) + '0');
+	}
+	*/
+	if (m < 10)
+	{
+	_putchar(':');
+	_putchar('0');
+	_putchar(m + '0');
+	}
+	/**
+	 * else (m >= 10)
+	{
+	_putchar(':')
+	_putchar((m / 10) + '0');
+	_putchar((m % 10) + '0');
+	}
+	*/
+	_putchar(h);
+	_putchar(':');
+	_putchar(m);
+	}
+	_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * _putchar: prints a character onto the std output
+ */
+
+void times_table(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+	{
+	_putchar((i * j));
+	_putchar('32');
+	_putchar('44');
+	}
+}
+_putchar('\n');
+}
