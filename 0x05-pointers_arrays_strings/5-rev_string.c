57991744f03e4e2ce40a0ca85b0936e58924c708
kadzaro1 <koblamens40@gmail.com>
2022-07-14 07:10:38 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string.
+ *
+ *@s: character parameter
+ *
+ *_putchar:
+ */
+void rev_string(char *s)
+{
+int i, j;
+j = strlen(s);
+for (i = j; i <= j; i--)
+	{
+	_putchar(s[i]);
+	}
+_putchar('\n');
+}
