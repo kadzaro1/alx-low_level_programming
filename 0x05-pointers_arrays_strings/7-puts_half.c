d8ec4a1a515e26c4ecfe8ba6ad0a37211cb21d06
kadzaro1 <koblamens40@gmail.com>
2022-07-15 07:51:59 -0700

    hot fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,18 @@
  */
 void puts_half(char *str)
 {
-int len1, len2, len3, len4;
+int len1, len2, len3;
 len1 = strlen(str);
 len2 = len1 / 2;
 len3 = len1 % 2;
-len4 = len1 - 1 / 2;
-	for (; len2 < len1; len2++)
-	{
+
 	if (len3 != 0)
-	_putchar(str[len4]);
-		else
-	_putchar(str[len2]);
-	}
+	len2++
+	else
+	len2
+for (; len2 < len1; len2++)
+{
+_putchar(str[len2]);
+}
 _putchar('\n');
 }
