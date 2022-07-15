1c2884be78c46b5aed8b00e64bf5899c00cfb5eb
kadzaro1 <koblamens40@gmail.com>
2022-07-15 08:41:36 -0700

    added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
+ *
+ *@a: array name
+ *@n: nth element
+ *
+ * _putchar: prints to the std output
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+_putchar('44');
+_putchar('32');
+}
+printf("\n");
+}
