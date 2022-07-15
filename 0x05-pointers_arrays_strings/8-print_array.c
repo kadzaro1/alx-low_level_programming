989541bd13c29d2f6999575d3a61fa3a5344cff9
kadzaro1 <koblamens40@gmail.com>
2022-07-15 10:16:35 -0700

    hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,6 @@
  *
  *@a: array name
  *@n: nth element
- *
- * _putchar: prints to the std output
  */
 
 void print_array(int *a, int n)
@@ -17,9 +15,14 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
+if (n - i != 1)
+{
 printf("%d", a[i]);
 printf(",");
 printf(" ");
+}
+else
+printf("%d", a[i]);
 }
 printf("\n");
 }
