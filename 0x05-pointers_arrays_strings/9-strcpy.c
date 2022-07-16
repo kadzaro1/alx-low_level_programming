320c909aec72f6520a2a6ac9368474640518ebf3
kadzaro1 <koblamens40@gmail.com>
2022-07-16 08:41:31 -0700

    fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,16 +13,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-	if (src[i] == "")
-	{
-	dest[i] = src[i];
-	}
-	dest[i] = src[i];
+int i = -1;
 
-}
+do {
+	i++;
+	dest[i] = src[i];
+}	while (src[i] != '\0);
 
 return (dest);
 }
