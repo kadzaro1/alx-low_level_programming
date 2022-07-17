58531435b26a4f0ee24596385b39383af096a157
kadzaro1 <koblamens40@gmail.com>
2022-07-17 09:47:37 -0700

    added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
+ *
+ *
+ * _putchar: prints char onto the std output
+ */
+
+void print_most_numbers(void)
+{
+char num;
+for (num = 0; num < 10; num++)
+{
+	if (num == 2 && num == 4)
+		num++;
+_putchar(num + '0');
+}
+_putchar('\n');
+}
