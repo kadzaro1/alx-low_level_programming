828b29b9af55e6ec3cc4f69fb766b1773ea41f45
kadzaro1 <koblamens40@gmail.com>
2022-07-17 10:07:06 -0700

    fixed 4-print_most_numbers.c and added 5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,9 @@ void print_most_numbers(void)
 char num;
 for (num = 0; num < 10; num++)
 {
-	if (num == 2 && num == 4)
-		num++;
-_putchar(num + '0');
-}
+	if (num != 2 && num != 4)
+	{
+		_putchar(num + '0');
+	}
 _putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ *
+ *
+ * _putchar: prints char to the std output
+ */
+
+void more_numbers(void)
+{
+char num1;
+char num2;
+char results;
+	for (num1 = 1; num1 <= 10; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num++)
+		{
+		results = _putchar(num2 + '0');
+		}
+	_putchar(results + '0');
+	}
+_putchar('\n');
+}
