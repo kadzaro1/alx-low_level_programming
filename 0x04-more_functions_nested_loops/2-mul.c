dd5b33928df9962d376187513b1d00706c9150dc
kadzaro1 <koblamens40@gmail.com>
2022-07-17 09:25:30 -0700

    added 2-mul.c and 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ *
+ *@a: int parameter
+ *@b: int parametr
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+int bot1 = a;
+int bot2 = b;
+return (bot1 * bot2);
+}
