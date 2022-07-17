66856765cd0a9d609aca058e280b7f8e74333e24
kadzaro1 <koblamens40@gmail.com>
2022-07-17 08:32:27 -0700

    added a new folder, main.h, readme fileand main.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase character.
+ *
+ *@c: character parameter
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+
+	return (1);
+else
+	return (0);
+}
